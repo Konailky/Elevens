9a5f65e970e30cfacb9f07000d88843fadb5f5f3
Konailky <32402567+Konailky@users.noreply.github.com>
2018-04-03 14:36:43 -0700

    4/3/18
    needs more work.

---
+++ Eleven's/DeckTester.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class DeckTester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DeckTester
+{
+    /**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+	}
+}
